fe5bc52c10ed621ab84878aa22061455113ef6d4
hashinmohammed <hashinpks@gmail.com>
2023-07-04 18:33:46 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import './App.css'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+// =================================
+// https://reactdatepicker.com/
+// =================================
+
+function App() {
+
+  const [selectedDate, setSelectedDate] = useState(null)
+  return (
+    <div className='App'>
+      <DatePicker
+        selected={selectedDate}
+        onChange={date => setSelectedDate(date)}
+        placeholderText={'dd/mm/yyyy'}
+        // minDate={new Date()}
+        // maxDate={new Date()}
+        filterDate={date => date.getDay() !== 6 && date.getDay() !== 0}
+        showYearDropdown
+        scrollableYearDropdown
+        showIcon
+        isClearable
+
+      />
+    </div>)
+
+}
+
+export default App
